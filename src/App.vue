5fbef9e61e06f0e9ccf3930633335d00b96a30f0
pmadrigal95 <pablomadrigal.1995@gmail.com>
2023-09-15 23:22:38 -0600

    initial commit
    base another one

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppHeaderVue from "@/components/AppHeader.vue";
+</script>
+
+<template>
+  <AppHeaderVue />
+  <div class="max-w-7xl p-10 m-auto">
+    <router-view />
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from "vue-router";
+import useAuthUser from "@/composables/UseAuthUser";
+
+const routes = [
+  {
+    name: "EmailConfirmation",
+    path: "/email-confirmation",
+    component: () => import("@/pages/EmailConfirmation.vue"),
+  },
+  {
+    name: "Home",
+    path: "/",
+    component: () => import("@/pages/Home.vue"),
+  },
+  {
+    name: "Me",
+    path: "/me",
+    meta: {
+      requiresAuth: true,
+    },
+    component: () => import("@/pages/Me.vue"),
+  },
+  {
+    name: "Login",
+    path: "/login",
+    component: () => import("@/pages/Login.vue"),
+  },
+  {
+    name: "ForgotPassword",
+    path: "/forgotPassword",
+    component: () => import("@/pages/ForgotPassword.vue"),
+  },
+  {
+    name: "Logout",
+    path: "/logout",
+    beforeEnter: async () => {
+      const { logout } = useAuthUser();
+      await logout();
+      return { name: "Home" };
+    },
+  },
+  {
+    name: "Register",
+    path: "/register",
+    component: () => import("@/pages/Register.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to) => {
+  const { isLoggedIn } = useAuthUser();
+  if (
+    !isLoggedIn() &&
+    to.meta.requiresAuth &&
+    !Object.keys(to.query).includes("fromEmail")
+  ) {
+    return { name: "Login" };
+  }
+});
+
+export default router;
